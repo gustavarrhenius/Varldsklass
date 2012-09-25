ae6003f916dffb01f27f8e78858e887e8043b018
Lasse Westerberg <westerberg.lasse@gmail.com>
2012-09-25 13:15:22 +0200

    Admin/News in progress

--- Varldsklass.Web/Controllers/AdminController.cs
+++ Varldsklass.Web/Controllers/AdminController.cs
@@ -3,8 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Varldsklass.Web.Models;
 
-namespace Lektion13.Web.Controllers
+namespace Varldsklass.Web.Controllers
 {
     public class AdminController : Controller
     {
@@ -19,9 +20,25 @@ namespace Lektion13.Web.Controllers
         {
             return View();
         }
+        [HttpGet]
         public ActionResult News()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult News(News news)
+        {
+            if (ModelState.IsValid)
+            {
+                // TODO: Email guestResponse to the part organizer
+                return View("OK", news);
+            }
+            else
+            {
+                // there is a validation error - redisplay the form
+                return View();
+            }
         }
         public ActionResult NewsLetter()
         {
@@ -32,7 +49,5 @@ namespace Lektion13.Web.Controllers
         {
             return View();
         }
-
-
     }
 }
