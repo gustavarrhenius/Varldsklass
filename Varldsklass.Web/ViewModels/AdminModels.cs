e0c3ffd89b300515208c5a066a35b5483fbc6e24
Caroline <norin.caroline@gmail.com>
2012-09-26 10:33:47 +0200

    Admin starkt typad vy

---
+++ Varldsklass.Web/ViewModels/AdminModels.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Varldsklass.Web.ViewModels
+    {
+    public class AdminModels
+        {
+
+        }
+    }
