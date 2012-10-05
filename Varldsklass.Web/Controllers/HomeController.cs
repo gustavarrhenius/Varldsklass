a084bb83a7a039072eb9b2250aa354a71019c373
Lasse Westerberg <westerberg.lasse@gmail.com>
2012-10-05 10:07:36 +0200

    Revert "Kontakta oss tillägg (åtgärd på Conflicted copies)"
    This reverts commit 3f4893bf8e37c47e767aed97577eacf1a92c6e58.

--- Varldsklass.Web/Controllers/HomeController.cs
+++ Varldsklass.Web/Controllers/HomeController.cs
@@ -15,16 +15,7 @@ using System.IO;
 =======
 using Varldsklass.Web.ViewModels;
 using Varldsklass.Domain.Contexts;
-<<<<<<< HEAD
 >>>>>>> Kontakta oss
-=======
-using System.Configuration;
-using DotNetOpenAuth.OAuth2;
-using DotNetOpenAuth.ApplicationBlock;
-using DotNetOpenAuth.ApplicationBlock.Facebook;
-using System.Net;
-using System.IO;
->>>>>>> Kontakta oss tillägg (åtgärd på Conflicted copies)
 
 namespace Varldsklass.Web.Controllers
 {
@@ -65,7 +56,6 @@ namespace Varldsklass.Web.Controllers
 
             var productsWithEmptyName = productRepo.FindAll(PostRepository
                                                             .FilterProductsWithEmptyDescription);
-<<<<<<< HEAD
             return View();
         }
 
@@ -76,23 +66,10 @@ namespace Varldsklass.Web.Controllers
             FacebookGraph.FacebookPageFeed graph = new FacebookGraph.FacebookPageFeed();
             if (null != facebookAccessToken)
                 {
-=======
-
-            return View();
-        }
-
-        public ActionResult FacebookFeed()
-        {
-            var facebookAccessToken = ConfigurationManager.AppSettings["facebookAccessToken"];
-            FacebookGraph.FacebookPageFeed graph = new FacebookGraph.FacebookPageFeed();
-            if (null != facebookAccessToken)
-            {
->>>>>>> Kontakta oss tillägg (åtgärd på Conflicted copies)
                 var request = WebRequest.Create(
                     string.Format(@"https://graph.facebook.com/varldsklass?fields=feed&access_token={0}",
                     Uri.EscapeDataString(facebookAccessToken)));
                 using (var response = request.GetResponse())
-<<<<<<< HEAD
                     {
                     using (var responseStream = response.GetResponseStream())
                         {
@@ -104,19 +81,6 @@ namespace Varldsklass.Web.Controllers
             return View(graph.PageFeed.Posts);
             }
 =======
-=======
-                {
-                    using (var responseStream = response.GetResponseStream())
-                    {
-                        graph = DotNetOpenAuth.ApplicationBlock.Facebook.FacebookGraph.FacebookPageFeed.Deserialize(responseStream);
-
-                    }
-                }
-            }
-            return View(graph.PageFeed.Posts);
-        }
-
->>>>>>> Kontakta oss tillägg (åtgärd på Conflicted copies)
         public ActionResult Contact()
         {
             return View();
