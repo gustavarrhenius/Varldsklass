86ab549ad6c52888e234572127e45397221dd72c
Gustav Arrhenius <gustav.arrhenius@gmail.com>
2012-10-05 10:46:16 +0200

    Event

---
+++ Varldsklass.Domain/Entities/Event.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Varldsklass.Domain.Entities
+{
+    class Event
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+
+        public int PostID { get; set; }
+        public virtual Post Post { get; set; }
+    }
+}
