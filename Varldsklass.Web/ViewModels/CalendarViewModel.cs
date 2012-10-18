6266eeb385e6dfa7cb1cc45606d7e4cd7384766a
Lasse Westerberg <westerberg.lasse@gmail.com>
2012-10-18 12:38:54 +0200

    Kalender
    Inte helt 100% på att det funkar. Behöver lite mer dummy data /LW

---
+++ Varldsklass.Web/ViewModels/CalendarViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Varldsklass.Domain.Entities;
+
+namespace Varldsklass.Web.ViewModels
+{
+    public class CalendarViewModel
+    {
+        public int ID { get; set; }
+        public String Title { get; set; }
+        public DateTime StartDate { get; set; }
+    }
+}
