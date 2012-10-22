db1070c02169b2e981344a8431d786b784deec92
Caroline <norin.caroline@gmail.com>
2012-10-22 14:44:29 +0200

    enkel sökfunktion implementerad

---
+++ Varldsklass.Domain/Entities/SearchResult.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Varldsklass.Domain.Entities
+    {
+    public class SearchResult
+        {
+        public string Title  { get; set; }
+        public string Url { get; set; }
+        public string Excerpt { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        }
+    }
