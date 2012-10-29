a38a611265f88cfb253c2ea1fe9337abab160b0b
Gustav Arrhenius <gustav.arrhenius@gmail.com>
2012-10-29 14:32:43 +0100

    Kartan i sliden, tillfällig lösning dock men javascript output

--- Varldsklass.Web/Content/js/js.js
+++ Varldsklass.Web/Content/js/js.js
@@ -19,10 +19,12 @@ function codeAddress() {
             map.setCenter(results[0].geometry.location);
             var marker = new google.maps.Marker({
                 map: map,
-                position: results[0].geometry.location
+                position: results[0].geometry.location,
+                clickable: true,
             });
         } else {
             alert("Geocode was not successful for the following reason: " + status);
         }
+
     });
 }
