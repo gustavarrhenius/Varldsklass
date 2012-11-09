ba0ebb6d84cf587a4812acd00abdeccebf787610
Prof. Odor Sleuth <christian.trommestad@gmail.com>
2012-11-09 09:08:18 +0100

    fler registreringsuppgifter

--- Varldsklass.Domain/Entities/Account.cs
+++ Varldsklass.Domain/Entities/Account.cs
@@ -12,6 +12,9 @@ namespace Varldsklass.Domain.Entities
         public string Email { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        public string Address { get; set; }
+        public string Company { get; set; }
+        public string Phone { get; set; }
         public string Password { get; set; }
         public string Salt { get; set; }
         public DateTime CreatedDate { get; set; }
